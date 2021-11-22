881ef01c85f12a5a162423bfa5fa46dbe1e7561c
Angel Terziev <angelterziev195@gmail.com>
2021-11-22 08:47:57 +0200

    'update'

--- script/script.js
+++ script/script.js
@@ -1,7 +1,43 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
-'use strict';
+"use strict";
 
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
+class ToDoApp {
+  submitButton = document.getElementById("submitButton");
+  inputData = document.getElementById("inputValue");
+  toDos = [];
+  toDoList = document.querySelector(".toDo-list");
+
+  constructor() {
+    this.addToDo();
+  }
+  // Methods
+  addToDo() {
+    this.submitButton.addEventListener("click", () => {
+      const data = { todo: this.inputData.value };
+      this.toDos.push(data);
+      this.renderToDo();
+      this.inputData.value = "";
+    });
+  }
+
+  renderToDo() {
+    if (this.toDos.length) {
+      this.toDoList.innerHTML = "";
+      this.toDos.forEach((el, i) => {
+        const html = `
+        <div class = "toDoWrap">
+        <span>${i + 1}. ${el.todo}</span>
+        <div class="button-container">
+          <button class = 'btn btn-success' type="button">Edit</button>
+          <button class = 'btn btn-danger' type="button">Delete</button>
+        </div>
+        </div>`;
+        this.toDoList.innerHTML += html;
+      });
+    }
+  }
+}
+
+window.onload = new ToDoApp();
