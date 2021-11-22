80c491f3e74c274b79371c414e2224b9f009fcfe
Angel Terziev <angelterziev195@gmail.com>
2021-11-22 06:32:26 +0200

    first commit

--- script/script.js
+++ script/script.js
@@ -5,23 +5,3 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-// SLIDER
-document.addEventListener('DOMContentLoaded', () => {
-	console.log('DOMContentLoaded');
-	const swiper = new Swiper('.swiper', {
-		// Optional parameters
-		loop: true,
-	
-		// Navigation arrows
-		navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
-	});	
-});
-
-// Mobile nav bar
-
-const hamburger = document.querySelector('#hamburger');
-const nav_ul = document.querySelector('#nav-ul');
-
-hamburger.addEventListener('click', () => {
-	nav_ul.classList.toggle('show');
-});
